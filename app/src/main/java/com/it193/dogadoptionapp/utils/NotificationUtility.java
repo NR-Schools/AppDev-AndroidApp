92daa21ef96a9a89d8d6dd277770402dbd58db01
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-01-27 18:26:06 +0800

    Fix toast on NotificationUtility

--- app/src/main/java/com/it193/dogadoptionapp/utils/NotificationUtility.java
+++ app/src/main/java/com/it193/dogadoptionapp/utils/NotificationUtility.java
@@ -1,6 +1,7 @@
 package com.it193.dogadoptionapp.utils;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -10,7 +11,7 @@ import com.it193.dogadoptionapp.R;
 public class NotificationUtility {
 
     public static void successAlert(Context ctx, String message) {
-        Toast.makeText(ctx, message, Toast.LENGTH_LONG);
+        Toast.makeText(ctx, message, Toast.LENGTH_LONG).show();
     }
 
     public static void errorAlert(Context ctx, String action, String message) {
@@ -19,6 +20,12 @@ public class NotificationUtility {
                 .setIcon(R.drawable.ic_launcher_foreground)
                 .setMessage(message + " Error")
                 .setCancelable(false)
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
                 .create()
                 .show();
     }
