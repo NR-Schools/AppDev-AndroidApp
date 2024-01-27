2fe9a49ddbf11a64085303c418e499b5a1f15ce1
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-01-27 18:31:21 +0800

    Tested LogIn is working

--- app/src/main/java/com/it193/dogadoptionapp/view/auth/LogInView.java
+++ app/src/main/java/com/it193/dogadoptionapp/view/auth/LogInView.java
@@ -2,7 +2,9 @@ package com.it193.dogadoptionapp.view.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,6 +13,7 @@ import com.it193.dogadoptionapp.model.Account;
 import com.it193.dogadoptionapp.retrofit.AccountApi;
 import com.it193.dogadoptionapp.retrofit.RetrofitService;
 import com.it193.dogadoptionapp.storage.AppStateStorage;
+import com.it193.dogadoptionapp.utils.AnimationUtility;
 import com.it193.dogadoptionapp.utils.InputUtility;
 import com.it193.dogadoptionapp.utils.NotificationUtility;
 
@@ -25,76 +28,87 @@ public class LogInView extends AppCompatActivity {
     private EditText emailField;
     private EditText passwordField;
     private Button logInButton;
+    private Button goToSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_view);
 
+        // Initialize AnimationUtility
+        AnimationUtility.getInstance().initialize(this, getLayoutInflater());
+
         // Initialize Retrofit
         RetrofitService retrofitService = new RetrofitService();
         accountApi = retrofitService.getRetrofit().create(AccountApi.class);
 
         // Get UI Fields
-        emailField = new EditText(this);
-        passwordField = new EditText(this);
-        logInButton = new Button(this);
+        emailField = findViewById(R.id.logInEmailField);
+        passwordField = findViewById(R.id.logInPasswordField);
+        logInButton = findViewById(R.id.logInButton);
+        goToSignUp = findViewById(R.id.logInViewToSignUpView);
 
         // Log In Action
-        handleLogInAction();
+        goToSignUp.setOnClickListener(this::handleSwitchToSignUp);
+        logInButton.setOnClickListener(this::handleLogInAction);
     }
 
-    private void handleLogInAction() {
-        // Handle LogIn Action
-        logInButton.setOnClickListener(v -> {
-            // Get Raw Data
-            String email = emailField.getText().toString();
-            String passwd = passwordField.getText().toString();
+    private void handleSwitchToSignUp(View v) {
+        // Start New Activity
+        startActivity(new Intent(LogInView.this, SignUpView.class));
+    }
 
-            // Check if all fields have non-empty input
-            if (InputUtility.stringsAreNotNullOrEmpty(
-                    email, passwd
-            )) {
-                NotificationUtility.errorAlert(
-                        this,
-                        "Log In",
-                        "Incomplete/Missing Inputs Found!!"
-                );
-                return;
-            }
+    private void handleLogInAction(View v) {
+        // Get Raw Data
+        String email = emailField.getText().toString();
+        String passwd = passwordField.getText().toString();
 
-            // Construct Model
-            Account existingAccount = new Account();
-            existingAccount.setEmail(email);
-            existingAccount.setPassword(passwd);
+        // Check if all fields have non-empty input
+        if (!InputUtility.stringsAreNotNullOrEmpty(
+                email, passwd
+        )) {
+            NotificationUtility.errorAlert(
+                    this,
+                    "Log In",
+                    "Incomplete/Missing Inputs Found!!"
+            );
+            return;
+        }
 
-            // Attempt LogIn
-            accountApi.userLogIn(existingAccount)
-                    .enqueue(new Callback<Account>() {
-                        @Override
-                        public void onResponse(Call<Account> call, Response<Account> response) {
-                            NotificationUtility.successAlert(
-                                    LogInView.this,
-                                    "Sign Up is Successful!"
-                            );
+        // Construct Model
+        Account existingAccount = new Account();
+        existingAccount.setEmail(email);
+        existingAccount.setPassword(passwd);
 
-                            // Save Account
-                            AppStateStorage
-                                    .getInstance()
-                                    .setActiveAccount(
-                                            response.body()
-                                    );
-                        }
+        // Attempt LogIn
+        AnimationUtility.getInstance().startLoading();
+        accountApi.userLogIn(existingAccount)
+                .enqueue(new Callback<Account>() {
+                    @Override
+                    public void onResponse(Call<Account> call, Response<Account> response) {
+                        AnimationUtility.getInstance().endLoading();
+                        NotificationUtility.successAlert(
+                                LogInView.this,
+                                "Sign Up is Successful!" + response.body().getSessionAuthString()
+                        );
 
-                        @Override
-                        public void onFailure(Call<Account> call, Throwable t) {
-                            NotificationUtility.errorAlert(
-                                    LogInView.this,
-                                    "Sign Up",
-                                    "Account Log In Attempt was Unsuccessful!"
-                            );
-                        }
-                    });
-        });
+                        // Save Account
+                        AppStateStorage
+                                .getInstance()
+                                .setActiveAccount(
+                                        response.body()
+                                );
+                    }
+
+                    @Override
+                    public void onFailure(Call<Account> call, Throwable t) {
+                        AnimationUtility.getInstance().endLoading();
+                        NotificationUtility.errorAlert(
+                                LogInView.this,
+                                "Sign Up",
+                                "Account Log In Attempt was Unsuccessful!"
+                        );
+                    }
+                });
     }
 }
