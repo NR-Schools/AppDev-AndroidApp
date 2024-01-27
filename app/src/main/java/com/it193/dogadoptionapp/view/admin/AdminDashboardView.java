0a1aa11ff0943f74d761a0a45d48e173025306df
Tuna <98098486+Null-Root@users.noreply.github.com>
2024-01-27 21:27:25 +0800

    Dump

--- app/src/main/java/com/it193/dogadoptionapp/view/admin/AdminDashboardView.java
+++ app/src/main/java/com/it193/dogadoptionapp/view/admin/AdminDashboardView.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
@@ -19,17 +20,38 @@ import androidx.navigation.ui.NavigationUI;
 import com.it193.dogadoptionapp.databinding.ActivityAdminDashboardViewBinding;
 
 import com.it193.dogadoptionapp.R;
+import com.it193.dogadoptionapp.retrofit.DogApi;
+import com.it193.dogadoptionapp.retrofit.RetrofitService;
 
 public class AdminDashboardView extends AppCompatActivity {
+
+    private DogApi dogApi;
+
+    private Button goToAddDog;
+    private Button goToUpdateDog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard_view);
 
-        Button goToAddDog = findViewById(R.id.adminDashboardViewToAddDogView);
-        Button goToUpdateDog = findViewById(R.id.adminDashboardViewToUpdateDogView);
+        // Initialize Retrofit
+        RetrofitService retrofitService = new RetrofitService();
+        dogApi = retrofitService.getRetrofit().create(DogApi.class);
 
+        // Initialize Components
+        initComponents();
+
+        // Handle Actions
+        handleRedirectActions();
+    }
+
+    private void initComponents() {
+        goToAddDog = findViewById(R.id.adminDashboardViewToAddDogView);
+        goToUpdateDog = findViewById(R.id.adminDashboardViewToUpdateDogView);
+    }
+
+    private void handleRedirectActions() {
         goToAddDog.setOnClickListener(v -> {
             startActivity(new Intent(AdminDashboardView.this, AddDogRecordView.class));
         });
